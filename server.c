cc50e80c151ed1cb2bc0707a11110a530cb075c7
jaechansong <jachan9824@gmail.com>
2022-07-17 23:58:14 +0900

    보안 server측 Socket Programming

--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-/* server.c */
+//Server측
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,83 +6,68 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
-#define POST 51     //Django Server Port
+#define PORT 51
 
 void error_handling(char *message);
 
-int main()            //Client측 Port를 입력받음-추후 수정(Port 고정하여 실행파일만 실행하는 방식으로)
+int main()
 {
-	int serv_sock;
-	int clnt_sock;
-	char buf[256];
-	struct sockaddr_in serv_addr;
-	struct sockaddr_in clnt_addr;
-	socklen_t clnt_addr_size;
+        int serv_sock;
+        int clnt_sock;
+        char buf[256];
+        struct sockaddr_in serv_addr;
+        struct sockaddr_in clnt_addr;
+        socklen_t clnt_addr_size;
+        char filename[1024];
+        char message[]="Hello World!";
 
-	char message[]="Send Security Mgnt";
-	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-	if(serv_sock == -1)
-		error_handling("socket() error");
-			
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(PORT);                        //Client측 Port(argv[1])
-	
-	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 )
-		error_handling("bind() error"); 
-	
-	if(listen(serv_sock, 5)==-1)
-		error_handling("listen() error");
-	
-	clnt_addr_size=sizeof(clnt_addr);  
-	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-	if(clnt_sock==-1)
-		error_handling("accept() error");  
-	
-	write(clnt_sock, message, sizeof(message));
-	int nbyte = 256;
+
+        serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+        if(serv_sock == -1)
+                error_handling("socket() error");
+
+        memset(&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family=AF_INET;
+        serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+        serv_addr.sin_port=htons(PORT);
+
+        if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 )
+                error_handling("bind() error");
+
+        if(listen(serv_sock, 5)==-1)
+                error_handling("listen() error");
+
+        clnt_addr_size=sizeof(clnt_addr);
+        clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
+        if(clnt_sock==-1)
+                error_handling("accept() error");
+
+        write(clnt_sock, message, sizeof(message));
+        int nbyte = 256;
     size_t filesize = 0, bufsize = 0;
     FILE *file = NULL;
 
-    file = fopen("aurora.jpg"/* 새로 만들 파일 이름 */, "wb");      //파일이름 Client측 파일이름으로 수정해야함
+    recv(clnt_sock, filename, 1024, 0);
 
-    //ntohl(filesize);
-	//recv filesize
+    file = fopen(filename/* 새로 만들 파일 이름 */, "wb");		//파일 이름 받기
 
-	//recv(clnt_sock, &filesize, sizeof(filesize), 0);
-    //ntohl(filesize);
-	//printf("file size = [%ld]\n", filesize);
     bufsize = 256;
 
-    while(/*filesize != 0*/nbyte!=0) {
- 		//if(filesize < 256) bufsize = filesize;
+    while(nbyte!=0) {
         nbyte = recv(clnt_sock, buf, bufsize, 0);
-		//printf("filesize:%ld nbyte: %d\n", filesize, nbyte);
-
- 		//filesize = filesize -nbyte;
-
-        fwrite(buf, sizeof(char), nbyte, file);		
-        //nbyte = 0;
+        fwrite(buf, sizeof(char), nbyte, file);
     }
- 
-/*
-	while((nbyte = recv(clnt_sock, buf, bufsize, 0) != 0)){
- 		fwrite(buf, sizeof(char), nbyte, file);
-	}
-*/	
 
-	fclose(file);
-	close(clnt_sock);	
-	close(serv_sock);
-	return 0;
+        fclose(file);
+        close(clnt_sock);
+        close(serv_sock);
+        return 0;
 }
 
 void error_handling(char *message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+        fputs(message, stderr);
+        fputc('\n', stderr);
+        exit(1);
 }
+                            
