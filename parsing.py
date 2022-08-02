85653e34e64ff256a133fd0f448f3fd72a0ae7c2
jaechansong <jachan9824@gmail.com>
2022-08-02 16:27:19 +0900

    보안 결과 txt->결과만 Parsing

---
+++ parsing.py
@@ -0,0 +1,18 @@
+import pprint
+
+result = []
+
+textfile = open("C:/Users/82102/Desktop/OIDC/test.txt", 'r')
+#textfile = open("/root/test.txt", 'r')              #배포용
+index = textfile.readlines()
+textfile.close()
+
+for i in range(len(index)):
+    if "----------- SCAN SUMMARY -----------" in index[i+2]:
+        break
+    result.append(index[i+1].replace(' FOUND\n',''))
+
+pprint.pprint(result)
+
+virus_num = len(result)
+print("Virus 감염수 : %s"%virus_num)
