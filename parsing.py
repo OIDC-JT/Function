ebc711e7867d5b4e48e66ce50e0e1f1bc57be4a4
jaechansong <jachan9824@gmail.com>
2022-08-04 00:17:24 +0900

    보안결수 txt Parsing

--- parsing.py
+++ parsing.py
@@ -1,18 +1,31 @@
 import pprint
+import os
 
-result = []
+def Parsing(ID, ServerID):
 
-textfile = open("C:/Users/82102/Desktop/OIDC/parsing_test.txt", 'r')
-#textfile = open("/root/test.txt", 'r')              #배포용
-index = textfile.readlines()
-textfile.close()
+    Txtfile_name = ID + '_' + ServerID
 
-for i in range(len(index)):
-    if "----------- SCAN SUMMARY -----------" in index[i+2]:
-        break
-    result.append(index[i+1].replace(' FOUND\n',''))
+    if os.path.exists('C:/Users/82102/Desktop/OIDC/parsing_test.txt'):             
+    #if os.path.exists("/root/%s.txt"%(Txtfile_name)):                    #배포용
+        result = []
+        
+        textfile = open("C:/Users/82102/Desktop/OIDC/parsing_test.txt", 'r')
+        #textfile = open("/root/%s.txt"%(Txtfile_name), 'r')              #배포용
+        index = textfile.readlines()
+        textfile.close()
 
-pprint.pprint(result)
+        for i in range(len(index)):
+            if "----------- SCAN SUMMARY -----------" in index[i+2]:
+                break
+            result.append(index[i+1].replace(' FOUND\n',''))
 
-virus_num = len(result)
-print("Virus 감염수 : %s"%virus_num)
+        pprint.pprint(result)
+
+        virus_num = len(result)
+        print("Virus 감염수 : %s"%virus_num)
+    else :
+        virus_num = "파일이 없습니다."
+        print(virus_num)
+
+
+Parsing('ID', 'ServerID')
